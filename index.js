909a66c71c75dbe44a35a20dc450c4c5d91d6218
StunnaDawg <jonsonallen9@gmail.com>
2023-03-26 17:06:19 -0300

    question addition

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const shapes = inquirer.prompt ([
         type: 'input', 
         message: 'Write 3 charcters',
         name: 'three'
+    }, 
+    
+    {
+        type: 'list', 
+        message: 'What shape do you want',
+        choices: ['Triangle', 'Square', 'Circle'],
+        name: 'shape'
+    }, 
+    {
+        type: 'input', 
+        message: 'Enter a colour name or Hexadecimal',
+        name: 'colour'
     }
 
 ]).then(answers => {
